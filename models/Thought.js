67b1ca47548c9038ec387a45ee43ea6b5dcf7a23
hellovolcano <valerie.gleason@gmail.com>
2022-08-29 15:42:49 -0500

    adding additional schema info

--- models/Thought.js
+++ models/Thought.js
@@ -1,15 +1,15 @@
 const { Schema, model } = require('mongoose')
 
 const ReactionSchema = new Schema({
-    // reaction schema goes here
-    // min: 1,
-    // max: 280
-})
-
-const ThoughtSchema = new Schema({
-    thoughtText: {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId
+    },
+    reactionBody: {
         type: String,
-        required: true
+        required: true,
+        min: 1,
+        max: 280
     },
     username: {
         type: String,
@@ -17,14 +17,47 @@ const ThoughtSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        // insert getter to make this date purdy
     }
-    // eventually add in a friends array that referenes itself
+    // min: 1,
+    // max: 280
 },
 {
     toJSON: {
-        virtuals: true
+        getters: true
     }
+})
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        // TODO: add a getter to make this date pretty
+    },
+    reactions: [ReactionSchema]
+    
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    }
+})
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    // return the link of the reactions array as reactionCount
+    return this.reactions.length
 })
 
 // creater the User model using the UserSchema
